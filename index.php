41b537906682f773bde20a8df0546fa567a0a970
Olivia Ringhiser <livvy16.olivia@gmail.com>
2020-03-10 00:59:51 -0400

    added html base

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<html>
+<h1>File Uploader</h1>
+<h3>Select a file to upload</h3>
+<h5>Valid types include .jpg and .gif</h5>
+<form action="#" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileToUpload">
+    <input type="submit" value="Upload File" name="submit">
+</form>
+</html>
