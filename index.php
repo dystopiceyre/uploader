e18af3e989b3c2f3d4ff99656b30f24fcfdce213
Olivia Ringhiser <livvy16.olivia@gmail.com>
2020-03-10 01:11:30 -0400

    remove

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require('/home/oringhis/uploadConfig.php');
 try {
-    $this->_db = new PDO(DB_UP_DSN, DB_UP_USERNAME, DB_UP_PASSWORD);
+    $db = new PDO(DB_UP_DSN, DB_UP_USERNAME, DB_UP_PASSWORD);
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
